f55d892950f4a5496ec3db92442056f914663cbc
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2018-08-05 02:24:05 +0900

    Fix issue #27
    Add English version.

---
+++ static/js/blocks/webapi_en.js
@@ -0,0 +1,24 @@
+Blockly.Msg.CATEGORY_GO             = "Go";
+Blockly.Msg.CATEGORY_STEERING_WHEEL = "Steering wheel";
+Blockly.Msg.CATEGORY_ELSE           = "else";
+
+Blockly.Msg.WEBAPI_INFO_MESSAGE    = "Get version";
+
+Blockly.Msg.WEBAPI_FORWARD_MESSAGE = "Go forward for %1 seconds(%2)";
+Blockly.Msg.WEBAPI_FORWARD_SLOW    = "slow";
+Blockly.Msg.WEBAPI_FORWARD_NORMAL  = "normal";
+Blockly.Msg.WEBAPI_FORWARD_HIGH    = "high";
+
+Blockly.Msg.WEBAPI_BACK_MESSAGE    = "Go backward for %1 seconds(%2)";
+Blockly.Msg.WEBAPI_BACK_SLOW       = "slow";
+Blockly.Msg.WEBAPI_BACK_NORMAL     = "normal";
+Blockly.Msg.WEBAPI_BACK_HIGH       = "high";
+
+Blockly.Msg.WEBAPI_DRIVE_MESSAGE   = "Go forward until %1 cm of the obstacle(%2)";
+Blockly.Msg.WEBAPI_DRIVE_SLOW      = "slow";
+Blockly.Msg.WEBAPI_DRIVE_NORMAL    = "normal";
+Blockly.Msg.WEBAPI_DRIVE_HIGH      = "high";
+
+Blockly.Msg.WEBAPI_TURN_RIGHT_MESSAGE = "Turn the steering wheel to the right";
+Blockly.Msg.WEBAPI_TURN_LEFT_MESSAGE  = "Turn the steering wheel to the left";
+Blockly.Msg.WEBAPI_TURN_FRONT_MESSAGE = "Return the steering wheel";
--- template_handler.go
+++ template_handler.go
@@ -8,21 +8,13 @@ import (
 )
 
 type templateHandler struct {
-	once          sync.Once
-	filename      string
-	templ         *template.Template
+	once     sync.Once
+	filename string
+	Lang     string
+	templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8080")
-//	w.Header().Set("Access-Control-Allow-Methods", "*")
-//	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
-//	w.Header().Add("Access-Control-Allow-Headers", "Origin")
-//	w.Header().Add("Access-Control-Allow-Headers", "X-Requested-With")
-//	w.Header().Add("Access-Control-Allow-Headers", "Accept")
-//	w.Header().Add("Access-Control-Allow-Headers", "Accept-Language")
-//	w.Header().Set("Content-Type", "application/json")
-
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(t.filename))
 	})
