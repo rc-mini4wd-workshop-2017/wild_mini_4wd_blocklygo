d149912087d8dd486efab9a5c2f9bb5c3d80ba4d
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2019-08-17 21:34:32 +0900

    Modify Kanji to Hiragana

--- static/js/blocks/webapi_ja.js
+++ static/js/blocks/webapi_ja.js
@@ -1,40 +1,40 @@
 Blockly.Msg.CATEGORY_VARIABLE       = "変数";
-Blockly.Msg.CATEGORY_LOOP           = "繰り返し";
-Blockly.Msg.CATEGORY_LOGIC          = "論理";
+Blockly.Msg.CATEGORY_LOOP           = "くりかえし";
+Blockly.Msg.CATEGORY_LOGIC          = "ろんり";
 Blockly.Msg.CATEGORY_CONTROL        = "コントロール";
-Blockly.Msg.CATEGORY_NUMBER         = "数";
+Blockly.Msg.CATEGORY_NUMBER         = "かず";
 
 Blockly.Msg.CATEGORY_MINI_4WD       = "ミニ四駆";
 
-Blockly.Msg.WEBAPI_INFO_MESSAGE    = "バージョン取得";
+Blockly.Msg.WEBAPI_INFO_MESSAGE    = "バージョン";
 
-Blockly.Msg.WEBAPI_GO_FORWARD_MESSAGE = "%1 前に進む";
+Blockly.Msg.WEBAPI_GO_FORWARD_MESSAGE = "%1 まえにすすむ";
 
-Blockly.Msg.WEBAPI_FORWARD_MESSAGE = "%1 秒 %2 前に進む";
-Blockly.Msg.WEBAPI_FORWARD_SLOW    = "遅く";
-Blockly.Msg.WEBAPI_FORWARD_NORMAL  = "普通に";
-Blockly.Msg.WEBAPI_FORWARD_HIGH    = "速く";
+Blockly.Msg.WEBAPI_FORWARD_MESSAGE = "%1 秒 %2 まえにすすむ";
+Blockly.Msg.WEBAPI_FORWARD_SLOW    = "おそく";
+Blockly.Msg.WEBAPI_FORWARD_NORMAL  = "ふつうに";
+Blockly.Msg.WEBAPI_FORWARD_HIGH    = "はやく";
 
-Blockly.Msg.WEBAPI_GO_BACK_MESSAGE = "%1 後ろに進む";
+Blockly.Msg.WEBAPI_GO_BACK_MESSAGE = "%1 うしろにすすむ";
 
-Blockly.Msg.WEBAPI_BACK_MESSAGE    = "%1 秒 %2 後ろに進む";
-Blockly.Msg.WEBAPI_BACK_SLOW       = "遅く";
-Blockly.Msg.WEBAPI_BACK_NORMAL     = "普通に";
-Blockly.Msg.WEBAPI_BACK_HIGH       = "速く";
+Blockly.Msg.WEBAPI_BACK_MESSAGE    = "%1 秒 %2 うしろにすすむ";
+Blockly.Msg.WEBAPI_BACK_SLOW       = "おそく";
+Blockly.Msg.WEBAPI_BACK_NORMAL     = "ふつうに";
+Blockly.Msg.WEBAPI_BACK_HIGH       = "はやく";
 
-Blockly.Msg.WEBAPI_DRIVE_MESSAGE   = "障害物の %1 cm前まで %2 進む";
-Blockly.Msg.WEBAPI_DRIVE_SLOW      = "遅く";
-Blockly.Msg.WEBAPI_DRIVE_NORMAL    = "普通に";
-Blockly.Msg.WEBAPI_DRIVE_HIGH      = "速く";
+Blockly.Msg.WEBAPI_DRIVE_MESSAGE   = "しょうがいぶつの %1 cmまえまで %2 すすむ";
+Blockly.Msg.WEBAPI_DRIVE_SLOW      = "おそく";
+Blockly.Msg.WEBAPI_DRIVE_NORMAL    = "ふつうに";
+Blockly.Msg.WEBAPI_DRIVE_HIGH      = "はやく";
 
-Blockly.Msg.WEBAPI_STOP_MESSAGE    = "止まる";
+Blockly.Msg.WEBAPI_STOP_MESSAGE    = "とまる";
 
-Blockly.Msg.WEBAPI_TURN_RIGHT_MESSAGE = "右にハンドルをきる";
-Blockly.Msg.WEBAPI_TURN_LEFT_MESSAGE  = "左にハンドルをきる";
-Blockly.Msg.WEBAPI_TURN_FRONT_MESSAGE = "ハンドルを戻す";
+Blockly.Msg.WEBAPI_TURN_RIGHT_MESSAGE = "右にハンドルをまわす";
+Blockly.Msg.WEBAPI_TURN_LEFT_MESSAGE  = "左にハンドルをまわる";
+Blockly.Msg.WEBAPI_TURN_FRONT_MESSAGE = "ハンドルをもどす";
 
-Blockly.Msg.WEBAPI_SERVO_MESSAGE   = "ハンドルをきる(%1°)";
+Blockly.Msg.WEBAPI_SERVO_MESSAGE   = "ハンドルをまわす(%1°)";
 
-Blockly.Msg.WEBAPI_IRGUN_MESSAGE   = "銃を撃つ";
+Blockly.Msg.WEBAPI_IRGUN_MESSAGE   = "じゅうをうつ";
 
-Blockly.Msg.WEBAPI_DISTANCE_MESSAGE   = "障害物までの距離";
+Blockly.Msg.WEBAPI_DISTANCE_MESSAGE   = "しょうがいぶつ までの きょり";
